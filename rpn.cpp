28bd14a76433a0de77d63fc9477b00a69284cb09
ARRRSEN2004 <amirarseniy@gmail.com>
2024-05-20 20:27:25 +0300

    Task6

---
+++ rpn.cpp
@@ -0,0 +1,39 @@
+#include "rpn.h"
+#include <stack>
+#include <sstream>
+
+double calculateRPN(const std::string& expr) {
+    std::stack<double> stack;
+    std::istringstream is(expr);
+
+    std::string token;
+    while (is >> token) {
+        if (tok == "+" || tok == "-" || tok == "*" || tok == "/") {
+            double op2 = stack.top();
+            stack.pop();
+            double op1 = stack.top();
+            stack.pop();
+
+            double result;
+            if (tok == "+") {
+                result = op1 + op2;
+            }
+            else if (tok == "-") {
+                result = op1 - op2;
+            }
+            else if (tok == "*") {
+                result = op1 * op2;
+            }
+            else if (tok == "/") {
+                result = op1 / op2;
+            }
+
+            stack.push(result);
+        }
+        else {
+            stack.push(std::stod(tok));
+        }
+    }
+
+    return stack.top();
+}
