a3f1633cb9c39ec3c8c0b70bdd3ab2b66b702be8
ARRRSEN2004 <amirarseniy@gmail.com>
2024-05-21 13:07:18 +0300

    Task8

--- kmp.h
+++ kmp.h
@@ -4,5 +4,5 @@
 
 using namespace std;
 
-std::vector<int> getIndices(const std::string& my_template, const std::string& text);
 vector<int> prefix_func(const string& s);
+std::vector<int> getIndices(const std::string& my_template, const std::string& text);
