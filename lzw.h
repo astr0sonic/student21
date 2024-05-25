39f4e6f449bd44b23068986fae767f47007ed6bc
ARRRSEN2004 <amirarseniy@gmail.com>
2024-05-26 00:50:43 +0300

    Task10

---
+++ lzw.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+std::vector<int> compress(const std::string& text);
+std::string decompress(const std::vector<int>& compressed);
