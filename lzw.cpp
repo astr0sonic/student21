ef27170bc66221b4f554e197448de423963a9d98
ARRRSEN2004 <amirarseniy@gmail.com>
2024-05-26 01:10:08 +0300

    Task10

--- lzw.cpp
+++ lzw.cpp
@@ -1,38 +1,32 @@
 #include "lzw.h"
 
-std::vector<int> compress(const std::string& text) {
+vector<int> compress(const string& text) {
     map<string, int> dict;
-    for (int i = 0; i < 256; i++)
-    {
+    for (int i = 0; i < 256; i++) {
         dict[string(1, char(i))] = i;
     }
     string curr = "";
     vector<int> compress;
-    for (char c : text)
-    {
+    for (char c : text) {
         string comb = curr + c;
-        if (dict.find(comb) != dict.end())
-        {
+        if (dict.find(comb) != dict.end()) {
             curr = comb;
         }
-        else
-        {
+        else {
             compress.push_back(dict[curr]);
             dict[comb] = dict.size();
             curr = string(1, c);
         }
     }
-    if (!curr.empty())
-    {
+    if (!curr.empty()) {
         compress.push_back(dict[curr]);
     }
     return compress;
 }
 
-std::string decompress(const std::vector<int>& compressed) {
+string decompress(const vector<int>& compressed) {
     map<int, string> dict;
-    for (int i = 0; i < 256; i++)
-    {
+    for (int i = 0; i < 256; i++) {
         dict[i] = string(1, char(i));
     }
     if (dict.find(compressed[0]) != dict.end()) {
@@ -42,15 +36,12 @@ std::string decompress(const std::vector<int>& compressed) {
         throw runtime_error("Invalid compressed data.");
     }
     string decompress = prev;
-    for (int i = 1; i < compressed.size(); i++)
-    {
+    for (int i = 1; i < compressed.size(); i++) {
         string curr;
-        if (dict.find(compressed[i]) != dict.end())
-        {
+        if (dict.find(compressed[i]) != dict.end()) {
             curr = dict[compressed[i]];
         }
-        else
-        {
+        else {
             curr = prev + prev[0];
         }
         decompress += curr;
